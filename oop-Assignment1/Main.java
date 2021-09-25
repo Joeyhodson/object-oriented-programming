b120868145561f2d3a9fb469e5ea26343bd71a00
Joey Hodson <joeyhodson32@gmail.com>
2021-09-25 18:50:53 -0400

    Finished main's switch block, removed scanner bugs, added tuition bonus method, added logic to check if a group has been entered, added a method to update a group, other bugs.

--- oop-Assignment1/Main.java
+++ oop-Assignment1/Main.java
@@ -15,19 +15,18 @@ Person
         -> Staff
 */
 
-
 public class Main {
 
     public static void main(String args[]) {
 
         Menu myMenu = new Menu();
         Scanner myScanner = new Scanner(System.in);
-        String name, id;
+        String name, id, department;
         int option;
 
         Student students[] = new Student[2];
-        Faculty faculty;
-        Staff staff;
+        Faculty faculty = null;
+        Staff staff = null;
 
 
         System.out.println("\t\t\tWelcome to my Personal Management Program");
@@ -41,10 +40,18 @@ public class Main {
 
                 case 1:
 
-                    // check if students are occupied
-                    String rank, department;
+                    // check if faculty is occupied
+                    if (faculty != null) {
+                        System.out.println("\tYou already have a Faculty member filled in. Do you want to update their information?");
+                        if (!(myMenu.updateOption())) {
+                            break;
+                        }
+                    }
 
-                    System.out.println("\n\nEnter faculty info:\n");
+                    // enter faculty info
+                    String rank;
+
+                    System.out.println("\n\nEnter faculty info:");
                     System.out.print("\t\tName of the faculty: ");
                     name = myScanner.nextLine();
 
@@ -67,12 +74,18 @@ public class Main {
 
                 case 2:
 
-                    // check if faculty is occupied
+                    if (students[0] != null) {
+                        System.out.println("\tYou already have two students filled in. Do you want to update their information?");
+                        if (!(myMenu.updateOption())) {
+                            break;
+                        }
+                    }
+
                     double gpa;
                     int currentCreditHours;
 
                     for (int i = 1; i <= 2; i++) {
-                        System.out.println("\nEnter student "+i+" info:\n");
+                        System.out.println("\nEnter student "+i+" info:");
                         System.out.print("\t\tName of student: ");
                         name = myScanner.nextLine();
 
@@ -92,19 +105,88 @@ public class Main {
                     break;
 
                 case 3:
-                    // check if students exist, ask for specific student
+
+                    // two students must be entered (i.e. if student[0] doesn't exist neither will student[1])
+                    if (students[0] == null) {
+                        System.out.println("\n\nSorry! No students have been entered yet\n");
+                        break;
+                    }
+
+                    int studentChoice;
+
+                    System.out.print("\nWhich student? Enter 1 for "+students[0].getName()+" or Enter 2 "+students[1].getName()+" ? ");
+                    studentChoice = myScanner.nextInt();
+
+                    if (studentChoice == 1 || studentChoice == 2) {
+                        System.out.println("\nHere is the tuition invoice for "+students[studentChoice-1].getName()+" :");
+                        students[studentChoice-1].displayTuition();
+                    }
+                    else {
+                        System.out.println("Invalid Choice, try again.");
+                    }
+                    
+                    myScanner.nextLine();
                     break;
 
                 case 4:
-                    // check if factulty exists
+
+                    if(faculty == null) {
+                        System.out.println("\n\nSorry! No Faculty member entered yet\n");
+                        break;
+                    }
+
+                    faculty.displayFaculty();
                     break;
 
                 case 5:
-                    // check if staff if occupied
+                    
+                    // check if staff is occupied
+                    if (staff != null) {
+                        System.out.println("\tYou already have a Staff member filled in. Do you want to update their information?");
+                        if (!(myMenu.updateOption())) {
+                            break;
+                        }
+                    }
+
+                    String status;
+
+                    System.out.println("\n\nEnter staff info:");
+                    System.out.print("\t\tName of the staff member: ");
+                    name = myScanner.nextLine();
+
+                    System.out.print("\t\tEnter the id: ");
+                    id = myScanner.nextLine();
+
+                    do {
+                        System.out.print("\t\tDepartment: ");
+                        department = myScanner.nextLine();
+                    } while (DepartmentPerson.isDepartmentValid(department.toLowerCase()) == false);
+
+                    do {
+                        System.out.print("\t\tStatus, Enter P for Part Time or Enter F for Full Time: ");
+                        status = myScanner.nextLine();
+                        status = status.toLowerCase();
+                    } while ((!(status.equals("p"))) && (!(status.equals("f"))));
+                    
+                    if (status.equals("p")) {
+                        status = "Part";
+                    }
+                    else {
+                        status = "Full";
+                    }
+
+                    staff = new Staff(name, id, department, status);
+                    System.out.print("\n\t\tStaff member added!\n\n");
                     break;
 
                 case 6:
-                    // check if staff exists
+
+                    if(staff == null) {
+                        System.out.println("\n\nSorry! No Staff member entered yet\n");
+                        break;
+                    }
+
+                    staff.displayStaff();
                     break;
 
                 case 7:
@@ -198,11 +280,18 @@ class Student extends Person {
     }
 
     public double getTuition() {
-        return currentCreditHours*creditHourPrice;
+        return (this.currentCreditHours)*creditHourPrice;
     }
 
     public double getTotalFees() {
         return getTuition()+standardFee;
+    }
+
+    public double getBonus() {
+        if (this.gpa >= 3.85) {
+            return 0.15*getTotalFees();
+        }
+        return 0;
     }
 
     public int getCurrentCreditHours() {
@@ -219,6 +308,20 @@ class Student extends Person {
 
     public void setGpa(double gpa) {
         this.gpa = gpa;
+    }
+
+    public void displayTuition() {
+        System.out.println("-----------------------------------------------------------------------");
+        System.out.println(this.getName()+"\t\t"+this.getId());
+        System.out.println("Credit Hours: "+this.getCurrentCreditHours()+" ($"+creditHourPrice+"/credit hour)");
+        System.out.print("Fees: $");
+        System.out.printf("%.0f", standardFee);
+        System.out.print("\n\nTotal payment (after discount): $");
+        System.out.printf("%.2f", (this.getTotalFees()-this.getBonus()));
+        System.out.print("\t($");
+        System.out.printf("%.0f", this.getBonus());
+        System.out.print(" discount applied)\n");
+        System.out.println("-----------------------------------------------------------------------");
     }
 }
 
@@ -251,6 +354,12 @@ class Faculty extends DepartmentPerson {
         return false;
     }
 
+    public void displayFaculty() {
+        System.out.println("-----------------------------------------------------------------------");
+        System.out.println(this.getName()+"\t\t"+this.getId());
+        System.out.println(this.getDepartment()+" Department, "+this.rank);
+        System.out.println("-----------------------------------------------------------------------");
+    }
 }
 
 
@@ -270,10 +379,19 @@ class Staff extends DepartmentPerson {
     public void setStatus(String status) {
         this.status = status;
     }
+
+    public void displayStaff() {
+        System.out.println("-----------------------------------------------------------------------");
+        System.out.println(this.getName()+"\t\t"+this.getId());
+        System.out.println(this.getDepartment()+" Department, "+this.status+" Time");
+        System.out.println("-----------------------------------------------------------------------");
+    }
 }
 
 
 class Menu {
+
+    Scanner myScanner = new Scanner(System.in);
 
     public int getOption() {
 
@@ -292,15 +410,28 @@ class Menu {
 
             System.out.print("\n\tEnter your selection: ");
 
-            // todo - save scanner object in a variable to avoid instaniating manyt times
-            option = (new Scanner(System.in)).nextLine();
+            option = myScanner.nextLine();
             intOption = Integer.parseInt(option);
 
             return intOption;
         }
 
         catch (NumberFormatException ex) {
-            return 0; // defaults in main switch block
+            return 0; // defaults in main's switch block
         }
+    }
+    
+    public boolean updateOption() {
+
+        String option;
+        do {
+            System.out.print("\tYes or No: ");
+            option = myScanner.nextLine();
+            option = option.toLowerCase();
+        } while ((!(option.equals("yes"))) && (!(option.equals("no"))));
+        if (option.equals("no")) {
+            return false;
+        }
+        return true;
     }
 }
