ebf9408229210f99dbf2f20081c59bfa97ff48d8
Joey Hodson <joeyhodson32@gmail.com>
2021-09-24 20:40:44 -0400

    Methods for various functions. Began case by case scenarios in the main method.

--- oop-Assignment1/Main.java
+++ oop-Assignment1/Main.java
@@ -3,21 +3,130 @@
 
 import java.util.*;
 
+/*
+Inheritance Structure: 
+
+Menu
+University? (act on all people at once)
+Person
+    -> Student
+    -> DepartmentPerson
+        -> Faculty
+        -> Staff
+*/
+
 
 public class Main {
 
     public static void main(String args[]) {
 
+        Menu myMenu = new Menu();
+        Scanner myScanner = new Scanner(System.in);
+        String name, id;
+        int option;
+
+        Student students[] = new Student[2];
+        Faculty faculty;
+        Staff staff;
+
+
+        System.out.println("\t\t\tWelcome to my Personal Management Program");
+        System.out.println("\n\nChoose one of the options:");
+
+        do {
+
+            System.out.println("");
+            option =  myMenu.getOption();
+            switch(option) {
+
+                case 1:
+
+                    // check if students are occupied
+                    String rank, department;
+
+                    System.out.println("\n\nEnter faculty info:\n");
+                    System.out.print("\t\tName of the faculty: ");
+                    name = myScanner.nextLine();
+
+                    System.out.print("\t\tID: ");
+                    id = myScanner.nextLine();
+
+                    do {
+                        System.out.print("\t\tRank: ");
+                        rank = myScanner.nextLine();
+                    } while (Faculty.isRankValid(rank.toLowerCase()) == false);
+
+                    do {
+                        System.out.print("\t\tDepartment: ");
+                        department = myScanner.nextLine();
+                    } while (DepartmentPerson.isDepartmentValid(department.toLowerCase()) == false);
+                    
+                    faculty = new Faculty(name, id, department, rank);
+                    System.out.print("\n\t\tFaculty successfully added!\n\n");
+                    break;
+
+                case 2:
+
+                    // check if faculty is occupied
+                    double gpa;
+                    int currentCreditHours;
+
+                    for (int i = 1; i <= 2; i++) {
+                        System.out.println("\nEnter student "+i+" info:\n");
+                        System.out.print("\t\tName of student: ");
+                        name = myScanner.nextLine();
+
+                        System.out.print("\t\tID: ");
+                        id = myScanner.nextLine();
+
+                        System.out.print("\t\tGpa: ");
+                        gpa = myScanner.nextDouble();
+
+                        System.out.print("\t\tCredit hours: ");
+                        currentCreditHours = myScanner.nextInt();
+
+                        students[i-1] = new Student(name, id, currentCreditHours, gpa);
+                        System.out.print("Thanks!\n\n");
+                        name = myScanner.nextLine(); // clears buffer
+                    }
+                    break;
+
+                case 3:
+                    // check if students exist, ask for specific student
+                    break;
+
+                case 4:
+                    // check if factulty exists
+                    break;
+
+                case 5:
+                    // check if staff if occupied
+                    break;
+
+                case 6:
+                    // check if staff exists
+                    break;
+
+                case 7:
+                    System.out.println("\n\n\n\nGoodbye!\n");
+                    break;
+
+                default:
+                    System.out.println("\nInvalid Entry - please try again\n");
+            }
+
+        } while (option != 7);
     }
 }
 
-
+/*
 public class University {
 
+    ArrayList<
 }
+*/
 
-
-public abstract class Person {
+abstract class Person {
 
     private String name, id;
 
@@ -44,7 +153,7 @@ public abstract class Person {
 }
 
 
-public abstract class DepartmentPerson extends Person {
+abstract class DepartmentPerson extends Person {
 
     private String department;
 
@@ -60,10 +169,21 @@ public abstract class DepartmentPerson extends Person {
     public void setDepartment(String department) {
         this.department = department;
     }
+
+    public static boolean isDepartmentValid(String department) {
+
+        if (department.equals("mathematics") || 
+            department.equals("engineering") || 
+            department.equals("english")) {
+            return true;
+        }
+        System.out.println("\t\t\t\""+department+"\" is invalid");
+        return false;
+    }
 }
 
 
-public class Student extends Person {
+class Student extends Person {
 
     private double creditHourPrice = 236.45;
     private double standardFee = 52;
@@ -71,7 +191,7 @@ public class Student extends Person {
     private int currentCreditHours;
     private double gpa;
 
-    public Student(String name, String id, int currentCreditHours, float gpa) {
+    public Student(String name, String id, int currentCreditHours, double gpa) {
         super(name, id);
         this.currentCreditHours = currentCreditHours;
         this.gpa = gpa;
@@ -103,7 +223,7 @@ public class Student extends Person {
 }
 
 
-public class Faculty extends DepartmentPerson {
+class Faculty extends DepartmentPerson {
 
     private String rank;
 
@@ -119,10 +239,22 @@ public class Faculty extends DepartmentPerson {
     public void setRank(String rank) {
         this.rank = rank;
     }
+
+    public static boolean isRankValid(String rank) {
+
+        if (rank.equals("professor") ||
+            rank.equals("dean") ||
+            rank.equals("researcher")) {
+            return true;
+        }
+        System.out.println("\t\t\t\""+rank+"\" is invalid");
+        return false;
+    }
+
 }
 
 
-public class Staff extends DepartmentPerson {
+class Staff extends DepartmentPerson {
 
     private String status;
 
@@ -141,25 +273,34 @@ public class Staff extends DepartmentPerson {
 }
 
 
-public class Menu {
+class Menu {
 
     public int getOption() {
 
-        int option;
+        try {
 
-        System.out.println("1- Enter the information of the faculty member");
-        System.out.println("2- Enter the information of the two students");
-        System.out.println("3- Print the tuition invoive");
-        System.out.println("4- Print faculty information");
-        System.out.println("5- Enter the information of the staff member");
-        System.out.println("6- Print the information of the staff member");
-        System.out.println("7- Exit Program");
+            String option;
+            int intOption;
 
-        System.out.print("\n\tEnter your selection: ");
+            System.out.println("1- Enter the information of the faculty member");
+            System.out.println("2- Enter the information of the two students");
+            System.out.println("3- Print tuition invoive");
+            System.out.println("4- Print faculty information");
+            System.out.println("5- Enter the information of the staff member");
+            System.out.println("6- Print the information of the staff member");
+            System.out.println("7- Exit Program");
 
-        // todo - save scanner object in a variable to avoid instaniating manyt times
-        option = (new Scanner(System.in)).nextInt();
+            System.out.print("\n\tEnter your selection: ");
 
-        return option;
+            // todo - save scanner object in a variable to avoid instaniating manyt times
+            option = (new Scanner(System.in)).nextLine();
+            intOption = Integer.parseInt(option);
+
+            return intOption;
+        }
+
+        catch (NumberFormatException ex) {
+            return 0; // defaults in main switch block
+        }
     }
 }
